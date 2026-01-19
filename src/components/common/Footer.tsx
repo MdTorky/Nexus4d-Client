98b7da0159f8311c2157348119375749b8c617cc
cargo <mohamed2003torky@gmail.com>
2026-01-19 23:35:36 +0800

    Added Newsletter 2

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 
 import { useTranslation } from 'react-i18next';
-import i18n from '../../i18n';
+// import i18n from '../../i18n';
 
 export default function Footer() {
     const { t } = useTranslation();
